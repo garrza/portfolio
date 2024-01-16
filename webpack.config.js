fe2f9d5d88afa82d06963da49b1e3bb65f837429
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2024-01-15 18:11:46 -0600

    Correct webpack configuration and presents, setup watch scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,39 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  devtool: "inline-source-map",
+  mode: "development",
   entry: "./src/index.js",
   output: {
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "./src/index.html"),
+      filename: "index.html",
     }),
-    new MiniCssExtractPlugin(),
-    new CssMinimizerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        test: /\.txt$/,
-        loader: "raw-loader",
-      },
-      {
-        test: /.s?css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
 };
